93a978b3c441c6540e58e656f05f5242f0a957a3
BryanSalas <bryans5252@sbcglobal.net>
2016-04-05 21:10:06 -0500

    update hello world

--- restaurantmacros/settings.py
+++ restaurantmacros/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
+    'restaurantmacros.app',
 )
 
 MIDDLEWARE_CLASSES = (
