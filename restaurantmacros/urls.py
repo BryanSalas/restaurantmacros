c4748cbb5c7c961a8b82db573b2df0921503877e
BryanSalas <bryans5252@sbcglobal.net>
2016-04-06 21:17:30 -0500

    added bootstrap and favicon

--- restaurantmacros/urls.py
+++ restaurantmacros/urls.py
@@ -1,11 +1,12 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
+from restaurantmacros.app import views
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
-    #url(r'^$', 'restaurantmacros.views.home', name='home'),
+    url(r'^$', views.home, name='home'),
     url(r'^polls/', include('restaurantmacros.app.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
