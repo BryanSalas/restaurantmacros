d2f5fa3c1da350e8d12a7488ea640816cdbb8817
BryanSalas <bryans5252@sbcglobal.net>
2016-04-08 23:08:16 -0500

    change form to only one input per macro

--- restaurantmacros/app/forms.py
+++ restaurantmacros/app/forms.py
@@ -2,21 +2,9 @@ from django import forms
 
 class SearchForm(forms.Form):
 
-    general_min = { 'min_value' : '0', 'label_suffix' : "", 'widget' : forms.NumberInput(attrs={'class': 'form-control', 'placeholder' : 'Min', 'style' : 'width : 68px'}) }
-    general_max = { 'min_value' : '0', 'label_suffix' : "", 'widget' : forms.NumberInput(attrs={'class': 'form-control', 'placeholder' : 'Max', 'style' : 'width : 68px'}), 'label':"-"}
+    general = { 'min_value' : '0', 'label_suffix' : "", 'widget' : forms.NumberInput(attrs={'class': 'form-control', 'placeholder' : 'Goal', 'style' : 'width : 71px'}) }
 
-    # calories
-    cal_min = forms.IntegerField(label = "Calories", **general_min)
-    cal_max = forms.IntegerField(**general_max)
-
-    # protein
-    pro_min = forms.IntegerField(label = "Protein", **general_min)
-    pro_max = forms.IntegerField(**general_max)
-
-    # fat
-    fat_min = forms.IntegerField(label = "Fat", **general_min)
-    fat_max = forms.IntegerField(**general_max)
-
-    # carbs
-    carb_min = forms.IntegerField(label = "Carbs", **general_min)
-    carb_max = forms.IntegerField(**general_max)
+    calories = forms.IntegerField(**general)
+    protein = forms.IntegerField(**general)
+    fat = forms.IntegerField(**general)
+    carbs = forms.IntegerField(**general)
