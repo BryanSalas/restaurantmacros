bca2669b9bec65aceaa96810ca6e1a52033455a4
BryanSalas <bryans5252@sbcglobal.net>
2016-04-08 21:34:47 -0500

    cleaned up forms

--- restaurantmacros/app/forms.py
+++ restaurantmacros/app/forms.py
@@ -1,11 +1,22 @@
 from django import forms
 
 class SearchForm(forms.Form):
-    min_cal = forms.IntegerField(min_value=0, label="Calories")
-    max_cal = forms.IntegerField(min_value=0, label="")
-    min_pro = forms.IntegerField(min_value=0)
-    max_pro = forms.IntegerField(min_value=0)
-    min_fat = forms.IntegerField(min_value=0)
-    max_fat = forms.IntegerField(min_value=0)
-    min_carb = forms.IntegerField(min_value=0)
-    max_carb = forms.IntegerField(min_value=0)
+
+    general_min = { 'min_value' : '0', 'label_suffix' : "", 'widget' : forms.NumberInput(attrs={'class': 'form-control', 'placeholder' : 'Min', 'style' : 'width : 67px'}) }
+    general_max = { 'min_value' : '0', 'label_suffix' : "", 'widget' : forms.NumberInput(attrs={'class': 'form-control', 'placeholder' : 'Max', 'style' : 'width : 67px'}), 'label':"-"}
+
+    # calories
+    cal_min = forms.IntegerField(label = "Calories", **general_min)
+    cal_max = forms.IntegerField(**general_max)
+
+    # protein
+    pro_min = forms.IntegerField(label = "Protein", **general_min)
+    pro_max = forms.IntegerField(**general_max)
+
+    # fat
+    fat_min = forms.IntegerField(label = "Fat", **general_min)
+    fat_max = forms.IntegerField(**general_max)
+
+    # carbs
+    carb_min = forms.IntegerField(label = "Carbs", **general_min)
+    carb_max = forms.IntegerField(**general_max)
