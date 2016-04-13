63ff9d19cfddb0df120239d08243a1e9ee564a1c
BryanSalas <bryans5252@sbcglobal.net>
2016-04-13 00:35:25 -0500

    added sorting, updated db

--- restaurantmacros/app/management/commands/update_db.py
+++ restaurantmacros/app/management/commands/update_db.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from restaurantmacros.app.models import Food, Restaurant
 from nutritionix import Nutritionix
+import sys
 
 APP_ID  = "dba98fde"
 API_KEY = "adbe45626226708ccb7d85830347aada"
@@ -10,27 +11,43 @@ nix = Nutritionix(app_id=APP_ID, api_key=API_KEY)
 class Command(BaseCommand):
     help = 'Updates the db with new info from Nutritionix'
 
-    def updateFoods(self, restaurant):
-        resp = nix.search('', brand_id=restaurant.brand_id, results="0:50").json()
-        for hit in resp['hits']:
-            item_name = hit['fields']['item_name']
-            item_id   = hit['fields']['item_id']
-            item = Food.objects.filter(name=item_name)
+    def updateFoods(self, restaurant, totalHits):
+        low_index = 0
+        high_index = 50
+        while(low_index <= totalHits):
+            resp = nix.search('', brand_id=restaurant.brand_id, results=str(low_index) + ":" + str(high_index)).json()
+            print(resp)
+            for hit in resp['hits']:
+                item_id   = hit['fields']['item_id']
+                item = Food.objects.filter(item_id=item_id)
 
-            # item not in DB
-            if item.count() != 1:
-                item_info = nix.item(id=item_id).json()
-                item_calories = item_info['nf_calories']
-                item_protein = item_info['nf_protein']
-                item_fat = item_info['nf_total_fat']
-                item_carbs = item_info['nf_total_carbohydrate']
-
-                newFood = Food(name=item_name, calories=item_calories, protein=item_protein, fat=item_fat, carbs=item_carbs, restaurant=restaurant)
-                newFood.save()
+                # item not in DB
+                if item.count() != 1:
+                    print("Adding " + str(hit['fields']) + " to db.")
+                    item_info = nix.item(id=item_id).json()
+                    try:
+                        item_name = hit['fields']['item_name']
+                        item_calories = item_info['nf_calories']
+                        item_protein = item_info['nf_protein']
+                        item_fat = item_info['nf_total_fat']
+                        item_carbs = item_info['nf_total_carbohydrate']
+                        item_id = item_info['item_id']
+                    except KeyError:
+                        print("Exiting script, API ran out of calls...")
+                        sys.exit()
+                    newFood = Food(name=item_name, calories=item_calories, protein=item_protein, fat=item_fat, carbs=item_carbs, restaurant=restaurant, item_id=item_id)
+                    newFood.save()
+            low_index = high_index
+            high_index += 50
 
     def handle(self, *args, **options):
         for restaurant in Restaurant.objects.all():
-            self.updateFoods(restaurant)
+            try:
+                totalHits = nix.search('', brand_id=restaurant.brand_id, results="0:1").json()['total_hits']
+            except KeyError:
+                print("Exiting script, API ran out of calls...")
+                sys.exit()
+            self.updateFoods(restaurant, totalHits)
 
 
 
--- restaurantmacros/app/views.py
+++ restaurantmacros/app/views.py
@@ -19,6 +19,6 @@ def results(request):
     fat = request.GET['fat']
     carbs = request.GET['carbs']
 
-    items = Food.objects.filter(calories__lte=calories).filter(protein__lte=protein).filter(fat__lte=fat).filter(carbs__lte=carbs)
+    items = Food.objects.filter(calories__lte=calories).filter(protein__lte=protein).filter(fat__lte=fat).filter(carbs__lte=carbs).order_by('restaurant', '-calories')
 
     return render(request, 'results.html', {'items' : items})
